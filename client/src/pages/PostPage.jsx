57814388add0b7142df0d03d9977abcc6f77f038
varun vishwakarma <varun@webwithvarun.com>
2024-05-11 19:29:32 +0530

    added post show functionality

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { Button, Spinner } from 'flowbite-react'
 
 const PostPage = () => {
+
+  const { postSlug } = useParams();
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+  const [post, setPost] = useState(null)
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        setLoading(true)
+        const res = await fetch(`/api/post/getposts?slug=${postSlug}`)
+        const data = await res.json();
+        if(!res.ok){
+          setError(true)
+          setLoading(false)
+        }
+        if(res.ok){
+          setPost(data.posts[0])
+          setLoading(false)
+          setError(false)
+        }
+      } catch (error) {
+        setError(true)
+        setLoading(false)
+        console.log(error.message)
+      }
+    }
+
+    fetchPost();
+  }, [postSlug])
+
+  if(loading) return (
+    <div className='flex justify-center items-center min-h-screen'>
+      <Spinner color='warning' size='lg' />
+    </div>
+  )
   return (
-    <div>PostPage</div>
+    <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
+      <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl underline'>{post && post.title}</h1>
+      <Link to={`/search?category=${post && post.category}`} className='self-center mt-5'>
+        <Button color='gray' pill size='xs'>{post && post.category}</Button>
+      </Link>
+      <img className='mt-10 p-3 max-h-[600px] w-full object-cover' src={post && post.image} alt="post image" />
+      <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-5xl text-sm'>
+        <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+        <span className='italic'>{post && (post.content.length / 1000).toFixed(0)} mins read</span>
+      </div>
+      <div className='p-3 max-w-5xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html: post && post.content}}>
+      </div>
+    </main>
   )
 }
 
