a63e8e03eb349a6dd7bb9e5b79b79da5fd988b2b
varun vishwakarma <varun@webwithvarun.com>
2024-05-19 00:28:53 +0530

    chaged 3

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.use((err, req, res, next) => {
 })
 
 connectDB().then(() => {
-    app.listen(3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
         console.log("Server is running on port 3000!");
     });
 }).catch((err) => {
